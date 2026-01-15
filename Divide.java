a166183727e4393f38b0aab8a7fdf0e1f518f8d9
Nikhil991127 <op.nikhil9911@gmail.com>
2026-01-15 22:51:05 +0530

    adding some divide and conquer question

--- Divide.java
+++ Divide.java
@@ -40,12 +40,56 @@ public class Divide {
             arr[i]=temp[k];
         }
     }
-    public static void main(String[] args) {
-        int arr[]={6,5,4,9,8,7};
-        printarr(arr);
-        mergesort(arr, 0, arr.length-1);
-        printarr(arr);
+    public static void quicksort(int arr[],int si,int ei){
+        if(si>=ei){
+            return;
+        }
+        int pivotidx=position( arr, si , ei);
+        quicksort(arr, si, pivotidx-1);
+        quicksort(arr, pivotidx+1, ei);
+    }
+    public static int position(int arr[],int si,int ei){
+        //System.err.println("entered in position");
+        int temp[]=new int[ei-si+1];
+        int i =si;
+        int pivot=ei;
+        int k=0;
+        //System.out.println("Stuck");
+       while(i<ei){
+        if(arr[i]<arr[pivot]){
+            temp[k]=arr[i];
+            //System.out.println("Swap 1");
+            k++;
+        }
+        i++;
+       }
+       //System.out.println("stuck");
+       temp[k]=arr[pivot];
+       pivot=k;
+       k++;
+       i=si;
+       while(i<ei){
+        if(arr[i]>temp[pivot]){
+            temp[k]=arr[i];
+            //System.out.println("Swap2");
+            k++;
+        }
+        i++;
+       }
+        for(k=0,i=si; k<temp.length ; i++,k++){
+            arr[i]=temp[k];
+        }
+        System.out.println(pivot);
+        return pivot;
 
+
+
+    }
+    public static void main(String[] args) {
+        int arr[]={6,5,4,8,7,9};
+        printarr(arr);
+        quicksort(arr, 0, arr.length-1);
+        printarr(arr);
         
     }
 }

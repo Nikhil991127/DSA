205b2e36a7067ad3c1b784f701c2320250f0529e
Nikhil991127 <op.nikhil9911@gmail.com>
2026-02-28 23:42:45 +0530

    some changes

--- Btree.java
+++ Btree.java
@@ -119,5 +119,6 @@ public class Btree{
         System.out.println(tree.sum(root));
         System.out.println(tree.sum(root));
         System.out.println(tree.sum(root));
+        System.out.println(tree.sum(root));
     }
 }  
